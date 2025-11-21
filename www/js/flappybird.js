878073e4377f3c55fd5e3567a745983ed5414d04
Your Name <you@example.com>
2025-11-21 23:13:13 +0800

    updates

--- www/js/flappybird.js
+++ www/js/flappybird.js
@@ -55,16 +55,16 @@ window.onload = function() {
 
     //load images
     birdImg = new Image();
-    birdImg.src = "./flappybird.png";
+    birdImg.src = "img/flappybird.png";
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
     topPipeImg = new Image();
-    topPipeImg.src = "./toppipe.png";
+    topPipeImg.src = "img/toppipe.png";
 
     bottomPipeImg = new Image();
-    bottomPipeImg.src = "./bottompipe.png";
+    bottomPipeImg.src = "img/bottompipe.png";
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); // every 1.5 seconds
@@ -112,7 +112,9 @@ function update() {
     velocityY += gravity;
     // bird.y += velocityY;
     bird.y = Math.max(bird.y + velocityY, 0); //apply gravity to current bird.y, limit the bird.y to top of the canvas
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    if (birdImg.complete && birdImg.naturalWidth > 0) {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
 
     if (bird.y > board.height) {
         gameOver = true;
@@ -122,7 +124,9 @@ function update() {
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
-        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+        if (pipe.img && pipe.img.complete && pipe.img.naturalWidth > 0) {
+            context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+        }
 
         if (!pipe.passed && bird.x > pipe.x + pipe.width) {
             score += 0.5; //0.5 because there are 2 pipes! so 0.5*2 = 1, 1 for each set of pipes
